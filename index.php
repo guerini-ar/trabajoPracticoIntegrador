387e774c666fb3e8d1db7ec9f89e324d8bfab803
guerini.ar <guerini.ar@gmail.com>
2023-04-17 15:51:17 -0300

    Subo algo de mi producción

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Estilos Bootstrap -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+
+  <!-- Estilos propios -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>TP_Integrador</title>
+</head>
+<body>
+
+  <main>
+
+    <!-- Barra de Navegación -->
+    <?php require_once 'navbar/navbar.html'; ?>
+
+    <!-- Carousel -->
+    <?php require_once 'carousel/carousel.html'; ?>
+
+    <!-- Cards -->
+    <?php require_once 'cards/cards.html'; ?>
+
+  </main>
+
+  <!-- JS Bootstrap -->
+  <script src="js/bootstrap.js"></script>
+</body>
+</html>
